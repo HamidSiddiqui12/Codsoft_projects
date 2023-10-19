e851c6c653f5b22ea897a6ddc7d9841c57f167a5
Hamid Siddiqui <hamidsiddiqui1222@gmail.com>
2023-10-19 21:01:06 +0530

    changes

--- Web_development_projects/Calculator_page/script.js
+++ Web_development_projects/Calculator_page/script.js
@@ -1,41 +1,41 @@
 let string = "";
-let display = document.getElementById('calc-display'); // Get the input field by its id
+let display = document.getElementById("calc-display"); // Get the input field by its id
 let inputs = document.querySelectorAll('input[type="button"]');
 
 Array.from(inputs).forEach((input) => {
-    input.addEventListener('click', (event) => {
-        const value = event.target.value;
-        
-        // Check if the clicked value is a symbol (+, -, *, /)
-        const isSymbol = /[\+\-\*\/]/.test(value);
+  input.addEventListener("click", (event) => {
+    const value = event.target.value;
 
-        // Check for consecutive symbols
-        if (isSymbol && /[\+\-\*\/]$/.test(string)) {
-            alert("Consecutive symbols are not allowed.");
-            return;
-        }
+    // Check if the clicked value is a symbol (+, -, *, /)
+    const isSymbol = /[\+\-\*\/]/.test(value);
 
-        if (value === '=') {
-            try {
-                string = eval(string);
-                display.value = string;
-            } catch (error) {
-                alert("Invalid expression");
-                string = "";
-                display.value = string;
-            }
-        } else if (value === 'AC') {
-            string = "";
-            display.value = string;
-        } else if (value === 'DE') {
-            if (string.length > 0) {
-                let newStr = string.slice(0, -1);
-                display.value = newStr;
-                string = newStr;
-            }
-        } else {
-            string = string + value;
-            display.value = string;
-        }
-    });
+    // Check for consecutive symbols
+    if (isSymbol && /[\+\-\*\/]$/.test(string)) {
+      alert("Consecutive symbols are not allowed.");
+      return;
+    }
+
+    if (value === "=") {
+      try {
+        string = eval(string);
+        display.value = string;
+      } catch (error) {
+        alert("Invalid expression");
+        string = "";
+        display.value = string;
+      }
+    } else if (value === "AC") {
+      string = "";
+      display.value = string;
+    } else if (value === "DE") {
+      if (string.length > 0) {
+        let newStr = string.slice(0, -1);
+        display.value = newStr;
+        string = newStr;
+      }
+    } else {
+      string = string + value;
+      display.value = string;
+    }
+  });
 });
